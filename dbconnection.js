26450e3c27a65ae4836fc356613e06afb7bb5771
Apptrader <proyectoapptrader@gmail.com>
2024-02-28 18:35:14 -0300

    s

--- dbconnection.js
+++ dbconnection.js
@@ -6,14 +6,13 @@ Exporta la instancia de Sequelize para que pueda ser utilizada en otros archivos
 */
 // dbconnection.js
 import { Sequelize } from 'sequelize';
-import {DB_HOST, FRONTEND_URL, DB_DATABASE, DB_USER,DB_PASSWORD } from './config.js'
+import { DB_HOST, FRONTEND_URL, DB_DATABASE, DB_USER, DB_PASSWORD } from './config.js';
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
   logging: false,
+  port: 5433 // Aquí se especifica el puerto de la base de datos
 });
-
-
 
 export default sequelize;
