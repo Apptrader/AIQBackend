76a348f23b819a09fd72fe020fca269e11dd15e9
Apptrader <proyectoapptrader@gmail.com>
2024-02-28 18:16:32 -0300

    s

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const TOKEN_SECRET = 'some secret key'
 
-export const DB_HOST = process.env.DB_HOST || '18.224.149.73'
+export const DB_HOST = process.env.DB_HOST || '3.141.28.86'
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 
 export const DB_DATABASE =process.env.DB_DATABASE || 'tradesite'
 export const DB_USER = process.env.DB_USER || 'postgres'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'cueva.dolar.mono'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'cuevadolarmono'
 
