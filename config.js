3ca1da95d8d9313c9c8e07b0be95ecb71eba2a92
Apptrader <proyectoapptrader@gmail.com>
2024-03-08 22:33:29 -0300

    sdad

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const TOKEN_SECRET = 'some secret key'
 
 export const DB_HOST = 'pruebaborrar1.cryueqiccrnl.us-east-2.rds.amazonaws.com'
-export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://frontend-viernes.s3-website.us-east-2.amazonaws.com'
 
 export const DB_DATABASE =process.env.DB_DATABASE || 'pruebaborrar'
 export const DB_USER = process.env.DB_USER || 'postgres'
