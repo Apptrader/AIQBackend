e9643a72271c734ceb53b2f80d15087ad89822e1
Apptrader <proyectoapptrader@gmail.com>
2024-03-20 02:14:20 -0300

    cambio 2

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const TOKEN_SECRET = 'some secret key'
 
-export const DB_HOST = 'pruebaborrar1.cryueqiccrnl.us-east-2.rds.amazonaws.com'
+export const DB_HOST = 'aiqlearning.cryueqiccrnl.us-east-2.rds.amazonaws.com'
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://frontend-viernes.s3-website.us-east-2.amazonaws.com'
 
-export const DB_DATABASE =process.env.DB_DATABASE || 'pruebaborrar'
+export const DB_DATABASE =process.env.DB_DATABASE || 'AIQAcademy'
 export const DB_USER = process.env.DB_USER || 'postgres'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres-borrar'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'AIQLearning#123!'
 
